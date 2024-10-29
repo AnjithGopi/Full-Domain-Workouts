400139dbdb409e3466f0cf2e5a48fc35c31ef402
AnjithGopi <ng.anjith444@gmail.com>
2024-10-29 15:23:57 +0530

    call apply and bind

--- JavaScript workouts/Basics.js
+++ JavaScript workouts/Basics.js
@@ -25,3 +25,39 @@ const proxy=new Proxy(user,handler)
 
 console.log(proxy.name)
 proxy.age=27
+
+
+// call apply and bind
+
+
+
+// in js call apply and bind are methods used to change the context ( this.value) of a function .
+//Each has its own way of handling arguments but achieves a similar goal of allowing methods and functions to be executed in different contexts.
+
+
+
+
+let person={
+    name:"Joel",
+    age:25,
+    sayHello(){
+        console.log("hello ",this.name)
+    }
+}
+
+
+
+let greetings= function greet(message){
+
+    console.log("Hello "+this.name+" "+message)
+}
+
+
+greetings.apply(person,["How are you"])
+
+
+greetings.call(person,"Poy oomb")
+
+person.sayHello()
+let newFunc=person.sayHello.bind(person)
+newFunc()
