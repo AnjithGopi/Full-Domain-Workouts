58709c01965a068c4df0eec6f3426171823a4b4c
AnjithGopi <ng.anjith444@gmail.com>
2024-10-30 12:40:23 +0530

    promise.all

--- JavaScript workouts/promise.js
+++ JavaScript workouts/promise.js
@@ -15,10 +15,10 @@ let myPromise = new Promise((resolve,reject)=>{
         let success=true
 
         if(success){
-            resolve("Task is completed")
+            resolve("Promise 1 resolved")
         }else{
 
-            reject("Task is rejected")
+            reject("promise 1 rejected")
         }
 
     },2000)
@@ -26,21 +26,61 @@ let myPromise = new Promise((resolve,reject)=>{
 
 
 
+let myPromise2=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        let success=true
+        if(!success){
+            resolve("Promise 2 resolved")
+        }else{
+            reject("Promise 2 rejected")
+        }
+    },2000)
+})
 
-myPromise
+
+
+let myPromise3=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        let success=true
+        if(success){
+            resolve("Promise 3 resolved")
+        }else{
+            reject("Promise 3 rejected")
+        }
+    },2000)
+})
+
+
+
+
+Promise.all([myPromise,myPromise2,myPromise3])  // promise .all example
+
+
 .then((result)=>{
     console.log(result)
-    return new Promise((resolve) => {    
-      resolve("Another task done")
+   
 
-    })
-}).then((result)=>{
-    console.log(result)
+    
 })
+
 .catch((error)=>{
     console.log(error)
 })
 
+
+
+
+// promise .race---------
+
+
+Promise.race([myPromise,myPromise2,myPromise3])
+
+.then((result)=>{
+    console.log("race resolved with:",result)
+})
+.catch((error)=>{
+    console.log("race rejected with:",error)
+})
 
 
 
