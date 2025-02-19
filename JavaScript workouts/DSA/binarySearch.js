5c9558b0c67d8b4cd0bcc8b15dd88dc260381d6c
AnjithGopi <ng.anjith444@gmail.com>
2025-02-19 14:31:10 +0530

    binary search added

--- JavaScript workouts/DSA/binarySearch.js
+++ JavaScript workouts/DSA/binarySearch.js
@@ -31,4 +31,4 @@ function binarySearch(arr,target){
 
 
 
-console.log(binarySearch([1,2,3,4,5,6],4))
+console.log(binarySearch([1,2,3,4,5,6],4))// binary dearch can be performed in sorted arrays only
