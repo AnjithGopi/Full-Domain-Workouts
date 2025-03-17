de69196b0881e93baa19402a4e5256ce8c35b9b5
AnjithGopi <ng.anjith444@gmail.com>
2025-03-17 16:46:37 +0530

    workouts

--- JavaScript workouts/functions.js
+++ JavaScript workouts/functions.js
@@ -1,2 +1,47 @@
 
 
+const ba = [23,5,7,3,5,2,87]
+// 87
+// 2 87
+// 5 2 87
+// 3 5 2 87
+// 7 3 5 2 87
+// 5,7,3,5,2,7
+// 23,5,7,3,5,2,87
+
+
+let num=""
+
+let rev=ba.reverse()
+
+for(let elem of rev){
+    
+    num=elem+" "+num
+    
+    console.log(num)
+}
+
+
+const products = [
+    {a:[4,34,343]},
+    {b:[333,334]},
+    {c:[34,343]},
+    {d:[23,4,23,32]}
+    ]
+    
+    let sum=[]
+    
+for(let elem of products){
+    
+    for(let element in elem){
+        
+        sum.push(elem[element].reduce((acc,curr)=>acc+curr,0))
+    }
+    
+    
+}
+
+
+let result=sum.reduce((acc,curr)=>acc+curr,0)
+
+console.log(result)
